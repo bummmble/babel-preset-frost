63c636b2534ca7162ae17b076bbfa23c3c0ddc11
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-12 14:07:03 -0600

    Adds basic repo structure

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['airbnb'],
+    rules: {
+        'promise/params-names': 1,
+        'import/no-unresolved': 0,
+        'import/named': 0,
+        'import/default': 0,
+        'import/no-commonjs': 0,
+        'import/prefer-default-export': 0,
+        'import/no-dynamic-require': 0,
+        'global-require': 0
+    }
+};
---
+++ build.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { rollup } = require('rollup');
+const builtinModules = require('builtin-modules');
+const args = require('minimist')(process.argv.slice(2));
+const pkg = require('./package.json');
+
+const external = Object.keys(pkg.dependencies).concat(builtinModules);
+
+function getFormatAndName() {
+  if (args.es) {
+    return { format: 'es', name: 'index.es.js' };
+  }
+  return { format: 'cjs', name: 'index.cjs.js' };
+}
+
+function build() {
+  const { format, name } = getFormatAndName();
+
+  return rollup({
+    entry: 'src/index.js',
+    external,
+  })
+    .then(({ write }) =>
+      write({
+        dest: name,
+        format,
+      }))
+    .then(() => console.log(`Babel-Preset-Frost built in ${format} format`))
+    .catch(err => console.error(err));
+}
+
+build();
